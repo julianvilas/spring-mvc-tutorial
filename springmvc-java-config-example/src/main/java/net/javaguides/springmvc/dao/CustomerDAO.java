9548359d882e518404f9a49b41ec557e852209e3
Julian Vilas <julian.vilas@gmail.com>
2022-04-01 16:15:27 +0200

    Remove all examples except the helloworld

--- springmvc-java-config-example/src/main/java/net/javaguides/springmvc/dao/CustomerDAO.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.springmvc.dao;
-
-import net.javaguides.springmvc.model.Customer;
-
-public interface CustomerDAO {
-	void saveCustomer(Customer theCustomer);
-}
--- springmvc-java-config-example/src/main/java/net/javaguides/springmvc/service/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package net.javaguides.springmvc.service;
-
-import net.javaguides.springmvc.model.Customer;
-
-public interface CustomerService {
-	public void saveCustomer(Customer theCustomer);
-}
